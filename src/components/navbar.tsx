37e29ee67e55d93e665101927854a4e36401867e
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-14 21:28:48 +0600

    feat: navbar design done

---
+++ src/components/navbar.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header className="w-full">
+      <nav className="max-w-screen-lg mx-auto px-4 bg-white pt-4">
+        <div className="w-full px-4 h-16 border border-zinc-200 shadow rounded-full flex items-center justify-between gap-4">
+          <Link to="/" className="text-xl font-medium tracking-tighter">
+            Guten Books
+          </Link>
+          <Link
+            to="/"
+            className="text-sm font-normal tracking-tighter px-4 py-2 rounded-full bg-blue-600 hover:bg-blue-500 text-white"
+          >
+            Wishlist
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
