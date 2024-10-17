4d96881ebb818ee8d01783d72e828d96fef15ea2
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-17 21:16:16 +0600

    feat: search functionality done

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,32 +1,48 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import BookCard from "../components/book-card";
 
 const HomePage = () => {
+  // api data states
   const [books, setBooks] = useState([]);
+  // loading state
   const [isLoading, setIsLoading] = useState(false);
+  // search state
+  const [search, setSearch] = useState("");
 
   // get books
-  const getBooks = async () => {
+  const getBooks = useCallback(async () => {
     setIsLoading(true);
-    const res = await fetch(`https://gutendex.com/books`);
+    const res = await fetch(`https://gutendex.com/books?search=${search}`);
     const data = await res.json();
     setBooks(data?.results);
     setIsLoading(false);
-  };
+  }, [search]);
 
   // fetch data
   useEffect(() => {
     getBooks();
-  }, []);
+  }, [getBooks, search]);
 
-  console.log(books);
+  // console.log(books);
 
   return (
     <section className="max-w-screen-xl mx-auto px-4 py-20 grid gap-8">
       <h1 className="text-3xl font-medium tracking-tighter">
         List of Gutenberg ebook.
       </h1>
+
+      {/* search and filter */}
+      <div className="w-full flex flex-col sm:flex-row sm:items-center justify-between gap-4">
+        {/* search */}
+        <input
+          type="text"
+          placeholder="Search by title"
+          className="px-4 py-2 rounded-full border border-zinc-200 focus:border-blue-600 outline-none w-full max-w-sm text-sm shadow bg-white"
+          onChange={(e) => setSearch(e?.target?.value)}
+        />
+      </div>
+
       {isLoading && <p className="py-4">Loading..</p>}
       <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
         {books &&
