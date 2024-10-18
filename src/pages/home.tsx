e32b564fd1e387eebdbeb4e1360069556540e43d
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-18 23:00:16 +0600

    feat: filter dropdown design and functionality done

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import BookCard from "../components/book-card";
-import { ArrowPathIcon } from "@heroicons/react/24/solid";
+import { ArrowPathIcon, ChevronDownIcon } from "@heroicons/react/24/solid";
 import useDebounce from "../hooks/useDebounce";
+import { topics } from "../constants";
 
 const HomePage = () => {
   // api data states
   const [books, setBooks] = useState([]);
   // loading state
   const [isLoading, setIsLoading] = useState(false);
-  // search & page state
+  // search, filter & page state
   const [search, setSearch] = useState("");
   const debouncedSearch = useDebounce(search);
+  const [filter, setFilter] = useState("");
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
   const [page, setPage] = useState(1);
+
+  // drop down ref
+  const dropDownRef = useRef(null);
 
   // get books
   const getBooks = useCallback(async () => {
@@ -21,8 +27,6 @@ const HomePage = () => {
       `https://gutendex.com/books?search=${debouncedSearch}&page=${page.toString()}`
     );
     const data = await res.json();
-    // console.log("data", data);
-
     setBooks(data?.results);
     setIsLoading(false);
   }, [debouncedSearch, page]);
@@ -31,6 +35,15 @@ const HomePage = () => {
   useEffect(() => {
     getBooks();
   }, [getBooks, search]);
+
+  useEffect(() => {
+    const close = (e: any) => {
+      if (dropDownRef?.current && !dropDownRef?.current?.contains(e?.target))
+        setIsFilterOpen(false);
+    };
+    document.addEventListener("mousedown", close);
+    return () => document.removeEventListener("mousedown", close);
+  }, []);
 
   console.log(books);
 
@@ -49,23 +62,52 @@ const HomePage = () => {
           className="px-4 py-2 rounded-full border border-zinc-200 focus:border-blue-600 outline-none w-full max-w-sm text-sm shadow bg-white"
           onChange={(e) => setSearch(e?.target?.value)}
         />
+        {/* filter */}
+        <div className="relative w-full max-w-40" ref={dropDownRef}>
+          <div
+            className="px-4 py-2 rounded-full border border-zinc-200 focus:border-blue-600 outline-none w-full text-sm shadow bg-white flex items-center justify-between gap-4 tracking-tighter text-zinc-500"
+            onClick={() => setIsFilterOpen(!isFilterOpen)}
+          >
+            <p className="truncate">{filter ? filter : "Filter by topic"}</p>
+            <ChevronDownIcon
+              className={`size-5 text-zinc-500 ${
+                isFilterOpen ? "rotate-180" : ""
+              }`}
+            />
+          </div>
+          {isFilterOpen && (
+            <ul className="absolute top-full mt-1 border border-zinc-200 shadow bg-white rounded-xl z-50 w-full max-h-40 overflow-y-auto list-none">
+              {topics?.map((topic) => (
+                <li
+                  className="px-4 py-2 text-sm text-zinc-500 truncate hover:bg-zinc-100 hover:cursor-pointer"
+                  onClick={() => {
+                    setFilter(topic);
+                    setIsFilterOpen(false);
+                  }}
+                >
+                  {topic}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
 
       {/* loader */}
-      {isLoading && (
+      {/* {isLoading && (
         <div className="flex items-center justify-center gap-1 mx-auto w-fit text-zinc-500">
           <ArrowPathIcon className="size-4 animate-spin" />
           <p className="text-sm">Loading..</p>
         </div>
-      )}
+      )} */}
 
       {/* book list */}
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
+      {/* <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
         {!isLoading &&
           books &&
           books?.length > 0 &&
           books?.map((book: any) => <BookCard key={book?.id} book={book} />)}
-      </div>
+      </div> */}
 
       {/* pagination */}
       <div className="flex items-center gap-2 justify-end">
